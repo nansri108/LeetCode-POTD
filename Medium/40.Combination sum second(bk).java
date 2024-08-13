1c53745193763f0e553998e5fb5f31a2b4341461
NANCY SRIVASTAVA <103940060+nansri108@users.noreply.github.com>
2024-08-13 16:43:02 +0530

    Create 40.Combination sum second(bk).java

---
+++ Medium/40.Combination sum second(bk).java
@@ -0,0 +1,22 @@
+class Solution {
+     public List<List<Integer>> combinationSum2(int[] cand, int target) {
+    Arrays.sort(cand);
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    List<Integer> path = new ArrayList<Integer>();
+    dfs(cand, 0, target, path, res);
+    return res;
+}
+void dfs(int[] cand, int cur, int target, List<Integer> path, List<List<Integer>> res) {
+    if (target == 0) {
+        res.add(new ArrayList(path));
+        return ;
+    }
+    if (target < 0) return;
+    for (int i = cur; i < cand.length; i++){
+        if (i > cur && cand[i] == cand[i-1]) continue;
+        path.add(path.size(), cand[i]);
+        dfs(cand, i+1, target - cand[i], path, res);
+        path.remove(path.size()-1);
+    }
+}
+}
